f2ffeb402598d293c0c0d03d711b578926fcab03
Faris Abulula <abululb02@gmail.com>
2024-04-02 18:19:10 +0100

    coding problem monday done

--- coding_problems/01-monday/monday.py
+++ coding_problems/01-monday/monday.py
@@ -1,2 +1,7 @@
 def missing(nums: list[int], s: str) -> str:
-    pass
+    code = "".join(s.lower().split())
+
+    if len(code) <= max(nums):
+        return "No mission today"
+
+    return "".join([code[num] for num in sorted(nums)])
