066c017da23feab9afbecab1bb98bd86f5b33383
Faris Abulula <abululb02@gmail.com>
2024-04-02 14:13:12 +0100

    coding problem solved

--- coding_problems/02-tuesday/tuesday.py
+++ coding_problems/02-tuesday/tuesday.py
@@ -1,2 +1,8 @@
+from collections import Counter
+
+
 def duplicate_count(text: str) -> int:
-    pass
+    return sum([1 for value in Counter(text.lower()).values() if value > 1])
+
+
+duplicate_count("abcdeaB")
