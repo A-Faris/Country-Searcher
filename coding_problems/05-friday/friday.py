b05ddf158f36e313e54b46c08754baff45ff9d39
Faris Abulula <abululb02@gmail.com>
2024-04-05 15:45:07 +0100

    coding problem done

--- coding_problems/05-friday/friday.py
+++ coding_problems/05-friday/friday.py
@@ -1,2 +1,12 @@
 def cleaned_counts(data: list[int]) -> list[int]:
-    pass
+    clean = [data[0]]
+    for num in data[1:]:
+        if num >= clean[-1]:
+            clean.append(num)
+        else:
+            clean.append(num+1)
+
+    return clean
+
+
+cleaned_counts([2, 1, 2])
