5ef720c6d76b17a9ba2eb47c0bc01b9e9f4404b0
Faris Abulula <abululb02@gmail.com>
2024-04-05 14:38:30 +0100

    done

---
+++ airports/test_airports.py
@@ -0,0 +1,4 @@
+import pytest
+from airport import save_as_json, load_weather_for_location, render_flights, get_flights_from_iata, load_airport_JSON, find_airports_from_name, table_values_from_iata, get_airport, load_argument
+
+def test_
